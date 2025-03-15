a122e9849f7512ae0d95c40c8fa6548e01b63be9
hridoy ahmed <sahmed221068@bscse.uiu.ac.bd>
2025-03-16 00:24:16 +0600

    up

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+// API routes
+const api = require("./api/main");
+app.use("/api", api);
+
 app.get("/", (req, res) => {
   res.status(200).json("Welcome, baazar limit app is working well");
 });
