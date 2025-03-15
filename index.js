1ac7b1b29e130af60328f35d5db65712bcfac72c
hridoy ahmed <sahmed221068@bscse.uiu.ac.bd>
2025-03-16 00:23:01 +0600

    up

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-app.get("/home", (req, res) => {
-  res.status(200).json("Welcome, baazar limited is working well");
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome, baazar limit app is working well");
 });
 
 app.listen(PORT, () => {
