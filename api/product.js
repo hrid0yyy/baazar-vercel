946ea02da6f59296676ed770f4c53f7a9160f846
hridoy ahmed <sahmed221068@bscse.uiu.ac.bd>
2025-03-16 23:50:00 +0600

    uploads

--- api/product.js
+++ api/product.js
@@ -5,7 +5,7 @@ const { supabase } = require("../supabase");
 const uploadImage = require("../utils/image");
 
 // Use memory storage for file uploads in serverless functions
-const upload = multer(); // Save files to the "uploads" directory
+const upload = multer({ storage: multer.memoryStorage() }); // Save files to the "uploads" directory
 
 /**
  * @swagger
